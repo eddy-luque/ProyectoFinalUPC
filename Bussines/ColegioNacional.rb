72819349e3550abdb27f3d685663862c9f4f25f8
Victor Quiñones <you@example.com>
2020-04-30 11:00:25 -0500

    Se añade metodo a la clase ColegioNacional

--- Bussines/ColegioNacional.rb
+++ Bussines/ColegioNacional.rb
@@ -15,4 +15,21 @@ class ColegioNacional < Alumno
 		end
 		return puntaje
 	end
+
+	def calcularPuntajeRE
+		if promedioPonderado >= 19
+			puntajere = 100
+		elsif promedioPonderado >= 18 and promedioPonderado < 19
+			puntaje = 80
+		elsif promedioPonderado >= 16 and promedioPonderado < 18
+			puntaje = 60
+		elsif promedioPonderado >= 14 and promedioPonderado < 16
+			puntaje = 40
+		elsif promedioPonderado >=11 and promedioPonderado < 14
+			puntaje = 20
+		elsif promedioPonderado < 11
+			puntaje = 0
+		end
+		return puntaje
+	end
 end
