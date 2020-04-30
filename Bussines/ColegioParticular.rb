f49c63e362521b8cd0a7b62de605cabe2ab03f86
Victor Quiñones <you@example.com>
2020-04-30 11:06:57 -0500

    Añadiendo método a la clase ColegioParticular

--- Bussines/ColegioParticular.rb
+++ Bussines/ColegioParticular.rb
@@ -8,4 +8,17 @@ class ColegioParticular < Alumno
 		@monto_pension = monto_pension
 		@puesto = puesto
 	end
+
+	def calcularPuntajeCS
+		if monto_pension <= 200
+			puntajecs = 90
+		elsif monto_pension > 200 and monto_pension <= 400
+			puntajecs = 70
+		elsif monto_pension > 400 and monto_pension <= 600
+			puntajecs = 50
+		elsif monto_pension > 600
+			puntajecs = 40
+		end
+		return puntajecs
+	end			 
 end
